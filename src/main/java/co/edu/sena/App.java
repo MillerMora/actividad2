5c6503a079e115fd745cc53cf5d45949d7e818de
Aprendiz <Aprendiz@BOGDFPCGMF4139.FormacionSena.RED>
2024-02-20 11:15:49 -0500

    primer ejercicio resuelto

--- src/main/java/co/edu/sena/App.java
+++ src/main/java/co/edu/sena/App.java
@@ -1,13 +1,21 @@
 package co.edu.sena;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.err.println("Calcular el area de un triángulo");
+        System.err.println("ingresa la base del triángulo: ");
+
+        Scanner src = new Scanner(System.in);
+        double base = src.nextDouble();
+        System.err.println("ingresa la altura del triángulo: ");
+        double height = src.nextDouble();
+
+        double result = (base * height) / 2;
+
+        System.err.println("El area del triángulo es: " + result);
     }
 }
