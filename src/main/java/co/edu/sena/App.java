da5c51a085467bc2daa2ade58853aa2757ccb1e4
Aprendiz <Aprendiz@BOGDFPCGMF4139.FormacionSena.RED>
2024-02-20 11:31:17 -0500

    Se agrego el segundo punto

--- src/main/java/co/edu/sena/App.java
+++ src/main/java/co/edu/sena/App.java
@@ -6,6 +6,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        // primer punto
         System.err.println("Calcular el area de un triángulo");
         System.err.println("ingresa la base del triángulo: ");
 
@@ -17,5 +18,21 @@ public class App
         double result = (base * height) / 2;
 
         System.err.println("El area del triángulo es: " + result);
+
+        // segundo punto
+
+        System.err.println("Suma de dos numeros");
+        System.err.println("ingresa el primer numero: ");
+        
+        Scanner addition = new Scanner(System.in);
+        float number1 = addition.nextFloat();
+        System.err.println("Ingresa el segundo numero: ");
+        float number2 = addition.nextFloat();
+
+        float result1 = number1 + number2 ;
+
+        System.err.println("La suma de los 2 numeros es: " + result1 );
+
+
     }
 }
