81310657283032b8385ec2c721a6b0e277599878
Aprendiz <Aprendiz@BOGDFPCGMF4139.FormacionSena.RED>
2024-02-20 11:45:12 -0500

    Se agrego el tercer punto

--- src/main/java/co/edu/sena/App.java
+++ src/main/java/co/edu/sena/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args )
     {
         // primer punto
-        System.err.println("Calcular el area de un triángulo");
+        System.err.println("Calcular el área de un triángulo");
         System.err.println("ingresa la base del triángulo: ");
 
         Scanner src = new Scanner(System.in);
@@ -17,22 +17,34 @@ public class App
 
         double result = (base * height) / 2;
 
-        System.err.println("El area del triángulo es: " + result);
+        System.err.println("El área del triángulo es: " + result);
 
         // segundo punto
 
-        System.err.println("Suma de dos numeros");
-        System.err.println("ingresa el primer numero: ");
+        System.err.println("Suma de dos números");
+        System.err.println("ingresa el primer número: ");
         
         Scanner addition = new Scanner(System.in);
         float number1 = addition.nextFloat();
-        System.err.println("Ingresa el segundo numero: ");
+        System.err.println("Ingresa el segundo número: ");
         float number2 = addition.nextFloat();
 
         float result1 = number1 + number2 ;
 
-        System.err.println("La suma de los 2 numeros es: " + result1 );
+        System.err.println("La suma de los 2 números es: " + result1 );
 
+        // Tercer punto
 
+        System.err.println("Número elevado al cuadrado");
+        System.err.println("Ingrese un número: ");
+
+        Scanner elevation = new Scanner(System.in);
+        double number3 = elevation.nextDouble();
+
+        double result3 = Math.pow(number3, 2);
+
+        System.err.println("El resultado es:" + result3);
+
+        
     }
 }
